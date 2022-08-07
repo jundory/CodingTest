a06471715329e3fbeef8b1eaf32b77d3f430f5d3
LeeJunSoo <ykwanlee98@naver.com>
2022-08-07 14:31:28 +0900

    commit final

--- homework4.js
+++ homework4.js
@@ -7,22 +7,6 @@
 // 2. a와 b는 0이상 100 이하입니다.
 // 3. a와 b는 같을 수 있거나, b가 크도록 대소관계가 정해져있습니다.
 
-// function solution(a,b,c) {
-
-//     let even = [];
-//     let result = 0;
-
-//     for (i=a; i<=b; i=i+c){   
-      
-//         if(i%2==0) {
-//             even.push(i);
-//         }      
-//             result ++
-//       }
-//       return even.length;
-//     }
-
-//   console.log (solution(4,50,3))
 
 
   function solution(a,b,c) {
@@ -32,8 +16,9 @@
       
         if(i%2==0) {
             even.push (i);
-        } 
-    }
+        } else if (a == b) {
+            return i;
+            }
+        }
     return even.length;
   }
-  console.log (solution(4,50,3))
